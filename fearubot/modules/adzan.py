ad60d7a4147841fc7d0cf0ed7729bc208c652707
FS-Project <fernans1.bot@gmail.vom>
2021-04-13 21:53:26 +0700

    REBASE TO FEARUBOT

---
+++ fearubot/modules/adzan.py
@@ -0,0 +1,62 @@
+# INFO : This is a copy of the source code from the One4ubot repo, and has the permission of the owner.
+import json
+
+import requests
+
+from fearubot import CMD_HELP
+from fearubot.events import register
+
+PLACE = ""
+
+
+@register(pattern="^.adzan(?: |$)(.*)")
+async def get_adzan(adzan):
+    if not adzan.pattern_match.group(1):
+        LOCATION = PLACE
+        if not LOCATION:
+            await adzan.edit("Harap tentukan kota atau negara bagian.")
+            return
+    else:
+        LOCATION = adzan.pattern_match.group(1)
+
+    # url = f'http://muslimsalat.com/{LOKASI}.json?key=bd099c5825cbedb9aa934e255a81a5fc'
+    url = f"https://api.pray.zone/v2/times/today.json?city={LOCATION}"
+    request = requests.get(url)
+    if request.status_code == 500:
+        return await adzan.edit(f"Tidak dapat menemukan kota `{LOCATION}`")
+
+    parsed = json.loads(request.text)
+
+    city = parsed["results"]["location"]["city"]
+    country = parsed["results"]["location"]["country"]
+    timezone = parsed["results"]["location"]["timezone"]
+    date = parsed["results"]["datetime"][0]["date"]["gregorian"]
+
+    imsak = parsed["results"]["datetime"][0]["times"]["Imsak"]
+    subuh = parsed["results"]["datetime"][0]["times"]["Fajr"]
+    zuhur = parsed["results"]["datetime"][0]["times"]["Dhuhr"]
+    ashar = parsed["results"]["datetime"][0]["times"]["Asr"]
+    maghrib = parsed["results"]["datetime"][0]["times"]["Maghrib"]
+    isya = parsed["results"]["datetime"][0]["times"]["Isha"]
+
+    result = (
+        f"**Jadwal Sholat**:\n"
+        f"📅 `{date} | {timezone}`\n"
+        f"📍 `{city} | {country}`\n\n"
+        f"**Imsak :** `{imsak}`\n"
+        f"**Subuh :** `{subuh}`\n"
+        f"**Zuhur :** `{zuhur}`\n"
+        f"**Ashar :** `{ashar}`\n"
+        f"**Maghrib :** `{maghrib}`\n"
+        f"**Isya :** `{isya}`\n"
+    )
+
+    await adzan.edit(result)
+
+
+CMD_HELP.update(
+    {
+        "adzan": ".adzan <kota>\
+        \nPenggunaan: Mendapat waktu sholat untuk muslim."
+    }
+)
